b6abfa27fd967d312352ee0d8f8bfe780a8a9e3c
Gras64 <gras64@web.de>
2019-04-18 00:50:27 +0200

    German

--- __init__.py
+++ __init__.py
@@ -4,10 +4,16 @@ from mycroft import MycroftSkill, intent_file_handler
 class PleaseRepeat(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        data = "ich"
+        self.add_event("utterance", self.puffer_data(data))
+
+    def puffer_data(self, data):
+        self.puffer = data
+        self.speak(data)
 
     @intent_file_handler('repeat.please.intent')
     def handle_repeat_please(self, message):
-        self.speak_dialog('repeat.please')
+        self.speak(self.puffer)
 
 
 def create_skill():
